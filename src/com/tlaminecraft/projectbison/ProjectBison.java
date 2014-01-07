adb5b7f2008c9ec051f6e3598db46d3b3af6d669
kingbirdy <theredking23@gmail.com>
2014-01-07 00:24:43 -0500

    remove vanilla recipes for PB items and implemented saving code (currently non-functional).

--- src/com/tlaminecraft/projectbison/ProjectBison.java
+++ src/com/tlaminecraft/projectbison/ProjectBison.java
@@ -1,5 +1,14 @@
 package com.tlaminecraft.projectbison;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ProjectBison extends JavaPlugin {
@@ -8,6 +17,24 @@ public class ProjectBison extends JavaPlugin {
 		getServer().getPluginManager().registerEvents(new GliderListener(), this);
 		getServer().getPluginManager().registerEvents(new ContainerListener(), this);
        
+		 Iterator<Recipe> recipes = Bukkit.recipeIterator();
+		 
+		 while(recipes.hasNext()) {
+			 Recipe recipe = recipes.next();
+			 if(recipe.getResult().equals(new ItemStack(Material.LEATHER_HELMET)) || recipe.getResult().equals(new ItemStack(Material.LEATHER_CHESTPLATE))
+					 || recipe.getResult().equals(new ItemStack(Material.LEATHER_LEGGINGS)) || recipe.getResult().equals(new ItemStack(Material.LEATHER_BOOTS))
+					 || recipe.getResult().equals(new ItemStack(Material.IRON_HELMET)) || recipe.getResult().equals(new ItemStack(Material.IRON_CHESTPLATE))
+					 || recipe.getResult().equals(new ItemStack(Material.IRON_LEGGINGS)) || recipe.getResult().equals(new ItemStack(Material.IRON_BOOTS))
+					 || recipe.getResult().equals(new ItemStack(Material.GOLD_HELMET)) || recipe.getResult().equals(new ItemStack(Material.GOLD_CHESTPLATE))
+					 || recipe.getResult().equals(new ItemStack(Material.GOLD_LEGGINGS)) || recipe.getResult().equals(new ItemStack(Material.GOLD_BOOTS))
+					 || recipe.getResult().equals(new ItemStack(Material.DIAMOND_HELMET)) || recipe.getResult().equals(new ItemStack(Material.DIAMOND_CHESTPLATE))
+					 || recipe.getResult().equals(new ItemStack(Material.DIAMOND_LEGGINGS)) || recipe.getResult().equals(new ItemStack(Material.DIAMOND_BOOTS))
+					 || recipe.getResult().equals(new ItemStack(Material.IRON_HOE)) || recipe.getResult().equals(new ItemStack(Material.GOLD_HOE))
+					 || recipe.getResult().equals(new ItemStack(Material.STONE_HOE)) || recipe.getResult().equals(new ItemStack(Material.DIAMOND_HOE))
+					 || recipe.getResult().equals(new ItemStack(Material.WOOD_SWORD)))
+				 recipes.remove();
+		 }
+		 
         //Adds Recipes
         getServer().addRecipe(Recipes.SWTArmor);
         getServer().addRecipe(Recipes.SWTLeggings);
@@ -30,14 +57,29 @@ public class ProjectBison extends JavaPlugin {
         getServer().addRecipe(Recipes.EKHelmet);
         getServer().addRecipe(Recipes.EKSpear);
         getServer().addRecipe(Recipes.Glider);
-
+        
+        //load inventories
+        try {
+			ContainerListener.crates = SLAPI.load("crates.bin");
+			this.getLogger().info("crates successfully loaded");
+		} catch (Exception e) {
+			this.getLogger().severe("Couldnt find/load crates.bin");
+			this.getLogger().severe("Creating new file");
+			ContainerListener.crates = new HashMap<Block, Inventory>();
+		}
 }
 
 	public void onDisable() {
 	
 	//Makes sure the Recipes are continually added every time the Plugin reloads	
 	getServer().clearRecipes();
-		
+	
+	//save inventories
+	try {
+		SLAPI.save(ContainerListener.crates, "crates.bin");
+	} catch (Exception e) {
+		this.getLogger().severe("Failed to save crates.bin");
+	}
 	}
 	
 }
